855f5275a69db6dc594a808bdcaa568f3ef234d0
Yaroslav <33199677+Ysroslav@users.noreply.github.com>
2019-01-20 01:25:13 +0300

    Update StudentSession.java

--- src/main/java/ru/bodrov/interview/lesson5/StudentSession.java
+++ src/main/java/ru/bodrov/interview/lesson5/StudentSession.java
@@ -13,53 +13,6 @@ import org.hibernate.cfg.Configuration;
 
 public class StudentSession {
 
-   /* private Session currentSession;
-
-    private Transaction currentTransaction;
-
-    public Session openCurrentSession() {
-        currentSession = getSessionFactory().openSession();
-        return currentSession;
-    }
-
-    public void closeCurrentSession() {
-        currentSession.close();
-    }
-
-    public Session openCurrentSessionTransaction() {
-        currentSession = getSessionFactory().openSession();
-        currentTransaction = currentSession.beginTransaction();
-        return currentSession;
-    }
-
-    public void closeCurrentSessionTransaction() {
-        currentTransaction.commit();
-        currentSession.close();
-    }
-
-    private static SessionFactory getSessionFactory(){
-        Configuration configuration = new Configuration().configure();
-        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-                .applySettings(configuration.getProperties());
-        SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
-        return sessionFactory;
-    }
-
-    public Session getCurrentSession() {
-        return currentSession;
-    }
-
-    public void setCurrentSession(Session currentSession) {
-        this.currentSession = currentSession;
-    }
-
-    public Transaction getCurrentTransaction() {
-        return currentTransaction;
-    }
-
-    public void setCurrentTransaction(Transaction currentTransaction) {
-        this.currentTransaction = currentTransaction;
-    }*/
    private static SessionFactory sessionFactory = buildSessionFactory();
 
     protected static SessionFactory buildSessionFactory() {
